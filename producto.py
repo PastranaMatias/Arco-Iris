63e592d3bfef0bd42b5e7a7ff73714e3e4bea730
Pastrana Matias <mpariel1997@gmail.com>
2026-09-06 23:41:51 -0300

    agrego atributo nuevo

--- producto.py
+++ producto.py
@@ -1,15 +1,16 @@
 class Producto:
-    def __init__(self, nombre, marca, precio, stock_inicial=0):
+    def __init__(self, nombre, marca, precio, color, stock_inicial=0,):
         self.nombre = nombre
         self.marca = marca
         self.precio = precio
+        self.color = color
         self.stock = stock_inicial
 
     def actualizar_stock(self, cantidad):
         self.stock += cantidad
 
     def mostrar_confirmacion(self):
-        print(f"✅ Producto: {self.nombre} ({self.marca}) - Precio: ${self.precio}, Stock: {self.stock}")
+        print(f"✅ Producto: {self.nombre} ({self.marca}) - Precio: ${self.precio}, Stock: {self.stock},Color:{self.color}")
 
     #Funcion para validar que le producto no tengo valores erroneos
     def validar_datos(self): 
@@ -18,7 +19,9 @@ class Producto:
 
         if not self.marca or self.marca.strip() == "":
             return False
-
+        
+        if not self.color or self.color.strip() == "":
+                    return False
         if self.precio <= 0:
             return False
 
